6df533c1d033c140ee8ba5ec1a9c8fdeb3a8ba5f
benqscholl <77950300+benqscholl@users.noreply.github.com>
2021-02-27 22:15:29 -0500

    Add files via upload

---
+++ pop-script.js
@@ -0,0 +1,8 @@
+function apologyFunc() {
+    let say = prompt('Have a go at apologizing—and make sure it comes from the heart!');
+      if(say) {
+          alert('Nice job. You just apologized to a computer, and while I would love to give you feedback, the author of this document did not encode a feedback response. Keep practicing!')
+    
+      }
+}
+    
